db9c78f725e8fd0b96cefc672f06f26be06d19ff
axlobanoff <axlobanoff@icloud.com>
2024-11-04 11:59:05 +0300

    Initial Commit

---
+++ HedonistApp/HedonistAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HedonistAppApp.swift
+//  HedonistApp
+//
+//  Created by Alexander Lobanoff on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct HedonistAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
